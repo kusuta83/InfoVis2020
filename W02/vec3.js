fe630b13a1f6c656569f84c387593f3e0195cbbf
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-13 14:50:31 +0900

    Create vec3.js

---
+++ W02/vec3.js
@@ -0,0 +1,12 @@
+// Add method
+Vec3.prototype.add = function (v) {
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+
+// Sum method
+Vec3.prototype.sum = function () {
+    return this.x + this.y + this.z;    
+}
