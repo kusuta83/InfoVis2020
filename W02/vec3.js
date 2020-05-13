c80d54d982a6acc683ee340356e9b4107ce98e4d
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-13 15:16:43 +0900

    Update vec3.js

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,12 +1,26 @@
-// Add method
-Vec3.prototype.add = function (v) {
-    this.x += v.x;
-    this.y += v.y;
-    this.z += v.z;
-    return this;
-}
+class Vec3 {
+    // Add method
+    add = function (v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+    }
 
-// Sum method
-Vec3.prototype.sum = function () {
-    return this.x + this.y + this.z;    
+    // Sum method
+    sum = function () {
+        return this.x + this.y + this.z;
+    }
+
+    min = function (v) {
+
+    }
+
+    mid = function (v) {
+
+    }
+
+    max = function (v) {
+        
+    }
 }
