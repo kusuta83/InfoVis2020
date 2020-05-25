d8a1d0b3cb78637d5993cf18a28ba469b4fedf5b
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-26 03:54:48 +0900

    add vartice and face in master

---
+++ W04/task1.js
@@ -0,0 +1,100 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var vertices = [
+        [ -1,  1, 0 ], // v0
+        [ -1, -1, 0 ], // v1
+        [  1, -1, 0 ]  // v2
+        [1, 1, 0] //v3
+        [ -1,  1, -1 ], // v4
+        [ -1, -1, -1 ], // v5
+        [  1, -1, -1 ]  // v6
+        [1, 1, -1] //v7
+    ];
+
+    var faces = [
+        [ 0, 1, 2 ] // f0
+        [0, 2, 3] //f1
+        [3, 2, 6]
+        [3, 6, 7]
+        [7, 6, 5] //f4
+        [7, 5, 4]
+        [4, 5, 1]
+        [4, 1, 0] //f7
+    ];
+
+    var v0 = new THREE.Vector3().fromArray( vertices[0] );
+    var v1 = new THREE.Vector3().fromArray( vertices[1] );
+    var v2 = new THREE.Vector3().fromArray( vertices[2] );
+    var v3 = new THREE.Vector3().fromArray( vertices[3] );
+    var v4 = new THREE.Vector3().fromArray( vertices[4] );
+    var v5 = new THREE.Vector3().fromArray( vertices[5] );
+    var v6 = new THREE.Vector3().fromArray( vertices[6] );
+    var v7 = new THREE.Vector3().fromArray( vertices[7] );
+    
+    var f = [];
+    for(var i = 0; i < 8; i++){
+        var id = faces[i];
+        f.add( new THREE.Face3( id[0], id[1], id[2] ));
+    }
+    // var id = faces[0];
+    // var f0 = new THREE.Face3( id[0], id[1], id[2] );
+    // id = faces[1];
+    // var f1 = new THREE.Face3( id[0], id[1], id[2] );
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push( v0 );
+    geometry.vertices.push( v1 );
+    geometry.vertices.push( v2 );
+    geometry.vertices.push( v3 );
+    geometry.vertices.push( v4 );
+    geometry.vertices.push( v5 );
+    geometry.vertices.push( v6 );
+    geometry.vertices.push( v7 );
+
+    f.forEach(function(face){
+        geometry.faces.push( face );
+    })
+    //geometry.faces.push( f0 );
+
+    //    var material = new THREE.MeshBasicMaterial();
+    var material = new THREE.MeshLambertMaterial();
+    material.vertexColors = THREE.FaceColors;
+    geometry.faces[0].color = new THREE.Color( 1, 0, 0 );
+
+    geometry.computeFaceNormals();
+    material.side = THREE.DoubleSide;
+
+    var triangle = new THREE.Mesh( geometry, material );
+    scene.add( triangle );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        triangle.rotation.x += 0.001;
+        triangle.rotation.y += 0.001;
+        renderer.render( scene, camera );
+    }
+}
