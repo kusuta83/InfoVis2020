0dbe8ce08bea01d5c545ef00c79d6f4861b85e07
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-26 04:49:18 +0900

    mouse__down_event to task2.js

--- W04/task2.js
+++ W04/task2.js
@@ -1,5 +1,4 @@
-function main()
-{
+function main() {
     var width = 500;
     var height = 500;
 
@@ -9,31 +8,31 @@ function main()
     var aspect = width / height;
     var near = 1;
     var far = 1000;
-    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );
-    scene.add( camera );
+    var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 0, 5);
+    scene.add(camera);
 
     var light = new THREE.PointLight();
-    light.position.set( 5, 5, 5 );
-    scene.add( light );
+    light.position.set(5, 5, 5);
+    scene.add(light);
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize(width, height);
+    document.body.appendChild(renderer.domElement);
 
     var vertices = [
-        [ -1,  1, 0 ], // v0
-        [ -1, -1, 0 ], // v1
-        [  1, -1, 0 ],  // v2
+        [-1, 1, 0], // v0
+        [-1, -1, 0], // v1
+        [1, -1, 0],  // v2
         [1, 1, 0], //v3
-        [ -1,  1, -2 ], // v4
-        [ -1, -1, -2 ], // v5
-        [  1, -1, -2 ],  // v6
+        [-1, 1, -2], // v4
+        [-1, -1, -2], // v5
+        [1, -1, -2],  // v6
         [1, 1, -2] //v7
     ];
 
     var faces = [
-        [ 0, 1, 2 ], // f0
+        [0, 1, 2], // f0
         [0, 2, 3], //f1
         [3, 2, 6],
         [3, 6, 7],
@@ -47,33 +46,33 @@ function main()
         [1, 6, 2] //f11
     ];
 
-    var v0 = new THREE.Vector3().fromArray( vertices[0] );
-    var v1 = new THREE.Vector3().fromArray( vertices[1] );
-    var v2 = new THREE.Vector3().fromArray( vertices[2] );
-    var v3 = new THREE.Vector3().fromArray( vertices[3] );
-    var v4 = new THREE.Vector3().fromArray( vertices[4] );
-    var v5 = new THREE.Vector3().fromArray( vertices[5] );
-    var v6 = new THREE.Vector3().fromArray( vertices[6] );
-    var v7 = new THREE.Vector3().fromArray( vertices[7] );
-    
+    var v0 = new THREE.Vector3().fromArray(vertices[0]);
+    var v1 = new THREE.Vector3().fromArray(vertices[1]);
+    var v2 = new THREE.Vector3().fromArray(vertices[2]);
+    var v3 = new THREE.Vector3().fromArray(vertices[3]);
+    var v4 = new THREE.Vector3().fromArray(vertices[4]);
+    var v5 = new THREE.Vector3().fromArray(vertices[5]);
+    var v6 = new THREE.Vector3().fromArray(vertices[6]);
+    var v7 = new THREE.Vector3().fromArray(vertices[7]);
+
     var f = [];
-    for(var i = 0; i < 12; i++){
+    for (var i = 0; i < 12; i++) {
         var id = faces[i];
-        f.push( new THREE.Face3( id[0], id[1], id[2] ));
+        f.push(new THREE.Face3(id[0], id[1], id[2]));
     }
 
     var geometry = new THREE.Geometry();
-    geometry.vertices.push( v0 );
-    geometry.vertices.push( v1 );
-    geometry.vertices.push( v2 );
-    geometry.vertices.push( v3 );
-    geometry.vertices.push( v4 );
-    geometry.vertices.push( v5 );
-    geometry.vertices.push( v6 );
-    geometry.vertices.push( v7 );
+    geometry.vertices.push(v0);
+    geometry.vertices.push(v1);
+    geometry.vertices.push(v2);
+    geometry.vertices.push(v3);
+    geometry.vertices.push(v4);
+    geometry.vertices.push(v5);
+    geometry.vertices.push(v6);
+    geometry.vertices.push(v7);
 
-    f.forEach(function(face){
-        geometry.faces.push( face );
+    f.forEach(function (face) {
+        geometry.faces.push(face);
     });
 
     //    var material = new THREE.MeshBasicMaterial();
@@ -84,16 +83,43 @@ function main()
     geometry.computeFaceNormals();
     material.side = THREE.DoubleSide;
 
-    var triangle = new THREE.Mesh( geometry, material );
-    scene.add( triangle );
+    var triangle = new THREE.Mesh(geometry, material);
+    scene.add(triangle);
 
     loop();
 
-    function loop()
-    {
-        requestAnimationFrame( loop );
+    function loop() {
+        requestAnimationFrame(loop);
         triangle.rotation.x += 0.005;
         triangle.rotation.y += 0.005;
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
+
+        document.addEventListener('mousedown', mouse_down_event);
+    }
+
+    function mouse_down_event(event) { // Mouse picking
+
+        var x_win = event.clientX;
+        var y_win = event.clientY;
+
+        var vx = renderer.domElement.offsetLeft;
+        var vy = renderer.domElement.offsetTop;
+        var vw = renderer.domElement.width;
+        var vh = renderer.domElement.height;
+        var x_NDC = 2 * (x_win - vx) / vw - 1;
+        var y_NDC = -(2 * (y_win - vy) / vh - 1);
+
+        var p_NDC = new THREE.Vector3(x_NDC, y_NDC, 1);
+        var p_wld = p_NDC.unproject(camera);
+
+        var origin = p_NDC;
+        var direction = p_wld;
+
+        var raycaster = new THREE.Raycaster(origin, direction);
+        var intersects = raycaster.intersectObject(triangle);
+        if (intersects.length > 0) {
+            intersects[0].face.color.setRGB(1, 0, 0);
+            intersects[0].object.geometry.colorsNeedUpdate = true;
+        }
     }
 }
