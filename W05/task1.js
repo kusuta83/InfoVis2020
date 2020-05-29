73377bc84f760bdfcb239e020cff76a7c804270d
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-29 21:08:00 +0900

    torus move

--- W05/task1.js
+++ W05/task1.js
@@ -1,5 +1,5 @@
 function main() {
-    var width = 500;
+    var width = 1000;
     var height = 500;
 
     var scene = new THREE.Scene();
@@ -42,9 +42,11 @@ function main() {
     });
 
     var torus_knot1 = new THREE.Mesh(geometry1, material1);
+    torus_knot1.position.set(-250, 0, 0);
     scene.add(torus_knot1);
 
     var torus_knot2 = new THREE.Mesh(geometry2, material2);
+    torus_knot2.position.set(250, 0, 0);
     scene.add(torus_knot2)
 
     loop();
