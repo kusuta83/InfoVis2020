321e9e85d79cce31b0f02ffc535ee81bd01f9aa0
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-05-29 22:43:57 +0900

    roughness

--- W05/task4.js
+++ W05/task4.js
@@ -29,7 +29,6 @@ function main() {
         uniforms: {
             light_position: { type: 'v3', value: light.position },
             camera_position: { type: 'v3', value: camera.position },
-            roughness: {type: 'f', value: roughness}
         }
     });
 
