284488ba1247142c2897a798539a0847c46185f2
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-06-01 21:39:48 +0900

    r 1.0

--- W06/task2.js
+++ W06/task2.js
@@ -42,7 +42,7 @@ function main()
     for ( var i = 0; i < 256; i++ )
     {
         var S = i / 255.0; // [0,1]
-        var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
+        var R = 1.0;
         var G = Math.max( Math.cos( S * Math.PI ), 0.0 );
         var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
         var color = new THREE.Color( R, G, B );
