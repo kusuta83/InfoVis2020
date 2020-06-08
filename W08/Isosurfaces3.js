0555ffb668d19df6a9abb0679c07464b7b26bedb
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-06-08 23:14:04 +0900

    torieaezu

--- W08/Isosurfaces3.js
+++ W08/Isosurfaces3.js
@@ -1,11 +1,14 @@
 function Isosurfaces(volume, isovalue) {
+
+
     var geometry = new THREE.Geometry();
     var material = new THREE.MeshLambertMaterial({
         vertexColors: THREE.VertexColors,
         vertexShader: document.getElementById('phong.vert').text,
         fragmentShader: document.getElementById('phong.frag').text,
         uniforms: {
-            light_position: { type: 'v3', value: light.position },
+            // light_position: { type: 'v3', value: light.position },
+            light_position: { type: 'v3', value: screen.camera.position},
             camera_position: { type: 'v3', value: camera.position }
         }
     });
