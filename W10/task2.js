3e75c4fe7b1122ccb2b3dc05213c137df6ecf05f
kusuta83 <65213235+kusuta83@users.noreply.github.com>
2020-06-15 07:38:45 +0900

    torima

--- W10/task2.js
+++ W10/task2.js
@@ -99,12 +99,9 @@ function TransferFunctionTexture() {
     for (var i = 0; i < resolution; i++) {
         var S = i / 255.0; // [0,1]
         var R = 1.0;
-        var G = 0.0;
-        var B = 0.0;
-        // var G = Math.max(1.0 - S, 0.0);
-        // var B = Math.max(1.0 - S, 0.0);
+        var G = Math.max(1.0 - S, 0.0);
+        var B = Math.max(1.0 - S, 0.0);
         // var color = new THREE.Color(R, G, B);
-        // var color = KVS.RainbowColorMap( 0, 255, i );
         var alpha = i / 255.0;
         data[4 * i + 0] = R;
         data[4 * i + 1] = G;
